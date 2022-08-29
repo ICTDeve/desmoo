e5fc457d70016095b2ea3efb539107853ceaf07d
victorribeiro1 <victorribeiro2929@gmail.com>
2022-08-29 20:55:48 -0300

    Inclusão da tabela "pesquisas" ao registro

---
+++ SQL/pesquisas.sql
@@ -0,0 +1,12 @@
+-- CONTROLE DE PESQUISAS DOS USUÁRIOS
+
+DROP TABLE IF EXISTS pesquisas;
+
+CREATE TABLE IF NOT EXISTS pesquisas (
+    id INT UNSIGNED NOT NULL,
+    id_usuario INT UNSIGNED NOT NULL,
+    conteudo_pesquisa VARCHAR (100) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_usuario)
+);
