f5ee73bbc7e3a0f46745f4aad72a855b9b8f1241
ProfessorPatinho <ds89e9@gmail.com>
2022-11-28 21:27:49 -0300

    IALA - Reconhecimento de voz e texto
    - Identação
    - Reconhecimento de voz e texto
    - Respostas
    - Pesquisa web

--- python/IALA/main.py
+++ python/IALA/main.py
@@ -1,44 +1,91 @@
 #Importando Bibliotecas necessários
 import pyautogui
-import speech_recognition as sr
-import sounddevice as sd
-import wavio as wv
-import pyttsx3
-import webbrowser
+import sounddevice as sd #Gravador Voz
+import wavio as wv # Leitura e gravação de arquivo .WAV
+import speech_recognition as sr #Conversor de audio em texto
+import pyttsx3 # Fala IALA
+import webbrowser # Navegação Web
 import random
 import os
 import time
 
 
+
+# Funções ----------------------------------------------------------
+
+# Gravação voz usuário
+def gravarVoz():
+    #Definindo configurações padrão de audio (Sound Device)
+    frequenciaAudio = 48000
+    sd.default.samplerate = frequenciaAudio
+    sd.default.channels = 2
+    # sd.default.sampwidth = 2
+    duration = 5
+    
+    gravacao = sd.rec(int(duration*frequenciaAudio),)
+    print("Comece a falar!")
+    sd.wait()
+    wv.write('minhavoz.wav', gravacao, frequenciaAudio, sampwidth=2)
+    print('Ok, processando...')
+
 # Voz IALA
-def iafala(fala):
+def vozIA(fala):
     engine = pyttsx3.init() #iniciar
     engine.say(fala) #Conteúdo de fla
     engine.runAndWait()
 
-iafala("Olá mozão, eu sou a inteligência artificial criada pelo meu mestre denominado Diego. Minha função é te dar amor e carinho quando o meu mestre bater as botas.")
+# Navegação Web
+def navegacaoWeb(fala):
+    frase = fala
+    print(f'debug {fala}')
+    search = frase.replace('Ir para', '')
+    webbrowser.open(f'https://www.google.com/search?q= {search}')
 
-def gravarVoz():
-    freq = 48000
-    duration = 5
-    gravacao = sd.rec(int(duration*freq),
-                        samplerate=freq, channels=2)
-    print("Comece a falar!")
-    sd.wait()
-    wv.write('minhavoz.mp3', gravacao, freq, sampwith=2)
-    print('Ok, processando...')
-
-while True:
-    def pesquisaggl():
-        frase = fala
-        search = frase.replace('Pesquisar', '')
-        search2 = search.replace('pesquisar', '')
-        webbrowser.open(f'https://www.google.com/search?q= {search2}')
-    gravarVoz()
-    r = sr.Recognizer
-    filename = 'minhavoz.mp3'
+def ConverterAudioEmTexto():
+    # Este Bloco irá Converter Voz em Texto
+    r = sr.Recognizer()
+    filename = 'minhavoz.wav'
     with sr.AudioFile(filename) as source:
+        global fraseConvertida 
         audio_data = r.record(source)
         text = r.recognize_google(audio_data, language='pt-br')
-        print('Você disse: {text}')
-    fala = text
+        print(f'Você disse: {text}')
+        fraseConvertida = text
+        # return fraseConvertida
+
+def respostaIA(fala):
+  
+    if fala == 'Bom dia':
+        vozIA('Bom dia para você também!')
+    
+    elif 'Ir para' in fala:
+        navegacaoWeb(fala)
+    
+    elif fala == 'Obrigado':
+        vozIA('De nada, é sempre um prazer em poder te ajudar macho!')
+    
+    elif fala == 'obrigado':
+        vozIA('De nada, é sempre um prazer em poder te ajudar macho!')
+      
+    elif fala == 'Obrigada':
+        vozIA('De nada, é sempre um prazer em poder te ajudar femea!')
+    
+    elif fala == 'obrigada':
+        vozIA('De nada, é sempre um prazer em poder te ajudar femea!')
+    
+    elif fala == 'Test':
+        vozIA('deu bom')
+
+#Iniciar ----------------------------------------------------------
+while True:
+    modoConversa = input(str('Deseja conversar por texto ou voz? \n'))
+
+    if modoConversa == 'voz':
+        # Iniciar Voz
+        gravarVoz()
+        ConverterAudioEmTexto()
+        respostaIA(fraseConvertida)
+
+    elif modoConversa == 'texto':
+        perguntaUsuario = input(str('Digite sua dúvida \n'))
+        respostaIA(perguntaUsuario)
